1d23b43f11061751ba3f49b6d33fde9aef4ff64e
PU-OL <pu-ol@gmx.de>
2025-11-28 21:45:19 +0100

    add index.php

---
+++ pudevelopment.tech/index.php
@@ -0,0 +1,20 @@
+<?php
+// Weiterleitung zu homepage.php
+$target = 'Homepage.php';
+
+if (!headers_sent()) {
+    header('Location: ' . $target, true, 302);
+    exit;
+}
+?>
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="refresh" content="0;url=<?php echo htmlspecialchars($target, ENT_QUOTES, 'UTF-8'); ?>">
+    <title>Weiterleitung...</title>
+</head>
+<body>
+    <p>Weiterleitung zu <a href="<?php echo htmlspecialchars($target, ENT_QUOTES, 'UTF-8'); ?>"><?php echo htmlspecialchars($target); ?></a></p>
+</body>
+</html>
