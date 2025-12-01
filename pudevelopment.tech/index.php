2f37bb91132be288043ac3d5cee7e532886b0970
PU-OL <pu-ol@gmx.de>
2025-12-01 18:19:16 +0100

    imporve index.php design

--- pudevelopment.tech/index.php
+++ pudevelopment.tech/index.php
@@ -1,53 +1,63 @@
 <!doctype html>
 <html lang="de">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="icon" type="image/jpg"
-          href="images/icons/cable-car.png">
-    <title>Home</title>
-    <link rel="stylesheet" href="css/stylesheet.css">
-</head>
-<body>
-    <header>
-        <h1><b>Home</b></h1>
-        <div id="logout">
-            <input type="submit" class="btn-teriträr" name="submit" value="Login" id="btn-login" >
-        </div>
-    </header>
-    
-    <main>
-        <table>
-            <tr>
-                <th id="cableway">Seilbahn</th>
-                <th>Pistengeräte</th>
-                <th>Beschneiungsanlagen</th>
-            </tr>
-            <tr>
-                <td id="cableway"><img src="images/seilbahnen/jochbahn.JPG" alt="Bild Seilbahn"></td>
-                <td><img src="images/pistengeraete/600W.jpg" alt="Bild Pistengerät"></td>
-                <td><img src="images/beschneiungsanlagen/tf10.jpg" alt="Bild Beschneiungsanlage"></td>
-            </tr>
-        </table>
-    </main>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/jpg"
+            href="images/icons/cable-car.png">
+        <title>Home</title>
+        <link rel="stylesheet" href="css/stylesheet.css">
+    </head>
+    <body>
+        <header>
+            <h1><b>Home</b></h1>
+            <div id="logout">
+                <input type="submit" class="btn-teriträr" name="submit" value="Login" id="btn-login" >
+            </div>
+        </header>
+        
+        <main>
+            <div id="seitenauswahl">
+                <h2>Unser Informationsangebot über: </h2>
+                <br>
+                <table>
+                    <tr>
+                        <th id="cableway">Seilbahn</th>
+                        <th id="slopevehicles">Pistengeräte</th>
+                        <th id="snowmaking">Beschneiungsanlagen</th>
+                    </tr>
+                    <tr>
+                        <td id="cableway"><img src="images/seilbahnen/jochbahn.JPG" alt="Bild Seilbahn" width="30vh"></td>
+                        <td id="slopevehicles"><img src="images/pistengeraete/600W.jpg" alt="Bild Pistengerät" width="30vh"></td>
+                        <td id="snowmaking"><img src="images/beschneiungsanlagen/tf10.jpg" alt="Bild Beschneiungsanlage" width="30vh"></td>
+                    </tr>
+                </table>
+            </div>
+        </main>
 
-    <footer>
-        <p>
-            <a href="Impressum.php">Impressum</a>
-        </p>
-        <p>&copy; 2024-2025 Philipp Uhlendorf</p>
-        <p>
-            <a href="Datenschutz.php">Datenschutz</a>
-        </p>
-    </footer>
-    <script>
-        document.getElementById('btn-login').addEventListener('click', function() {//Zum Login wechseln
-                window.location.href = 'login.php';
-            });
+        <footer>
+            <p>
+                <a href="Impressum.php">Impressum</a>
+            </p>
+            <p>&copy; 2024-2025 Philipp Uhlendorf</p>
+            <p>
+                <a href="Datenschutz.php">Datenschutz</a>
+            </p>
+        </footer>
+        <script>
+            document.getElementById('btn-login').addEventListener('click', function() {//Zum Login wechseln
+                    window.location.href = 'login.php';
+                });
 
-        document.getElementById('cableway').addEventListener('click', function() {//Zur Seilbahn Seite wechseln
-                window.location.href = 'seilbahnen.php';
-            });
-    </script>
-</body>
+            document.getElementById('cableway').addEventListener('click', function() {//Zur Seilbahn Seite wechseln
+                    window.location.href = 'seilbahnen.php';
+                });
+            document.getElementById('slopevehicles').addEventListener('click', function() {//Zur Pistengeräte Seite wechseln
+                    alert("Seite nicht verfügbar");
+                });
+            document.getElementById('snowmaking').addEventListener('click', function() {//Zur Beschneiungsanlagen Seite wechseln
+                    alert("Seite nicht verfügbar");
+                });
+        </script>
+    </body>
 </html>
